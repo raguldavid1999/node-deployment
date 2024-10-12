ccef5651f4b3eb477dfe2bb2e1817fa165ee5c57
raguldavid1999 <raguldavid1999@gmail.com>
2024-10-13 00:29:05 +0530

    database connection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express()
-
+const app = express();
+const { Pool } = require('pg');
+require('dotenv').config();
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
 const users = [
     {
         id: 1,
@@ -14,8 +16,22 @@ const users = [
     }
 ]
 
-app.get('/users',(req, res)=>{
-    res.send(users);
+const itemsPool = new Pool({
+    connectionString: 'postgresql://root:fRd3GVDon8jexZfnArep4jmPBd3Q41nT@dpg-cs57pag8fa8c73ahjveg-a.oregon-postgres.render.com/test_database_x1mv',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+app.post('/users',async (req, res)=>{
+    try{
+        const params = req.body;
+        const result = await itemsPool.query(`SELECT * FROM public.users WHERE first_name = '${params.first_name}'`);
+        res.json(result.rows);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
 })
 
 app.get('/users/:id', (req, res)=>{
@@ -26,6 +42,6 @@ app.get('/users/:id', (req, res)=>{
     res.send(user);
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('User service is running');
 })
